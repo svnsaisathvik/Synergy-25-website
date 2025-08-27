d689ff442b0d2dd0474749901e6007593c69c31f
SathishAdithiyaaSV <sathishadithiyaa.sv@iiitb.ac.in>
2025-08-27 18:24:20 +0530

    Update dist folder

--- dist/assets/createLucideIcon-cZBiOrd4.js
+++ dist/assets/createLucideIcon-cZBiOrd4.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-B7vZXuMf.js";/**
+import{r as s}from"./index-Cm9GZo0Q.js";/**
  * @license lucide-react v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.

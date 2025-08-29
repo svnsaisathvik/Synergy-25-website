c7903141e0c782f328d648e941fea4e66e0644d7
SathishAdithiyaaSV <sathishadithiyaa.sv@iiitb.ac.in>
2025-08-29 15:55:16 +0530

    Build push

--- dist/assets/createLucideIcon-B08dx0jQ.js
+++ dist/assets/createLucideIcon-B08dx0jQ.js
@@ -1,4 +1,4 @@
-import{r as s}from"./index-CjsUcRPN.js";/**
+import{r as s}from"./index-CpUrd0k8.js";/**
  * @license lucide-react v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
